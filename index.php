38b6f92ae1ea4a2dd6610cde4bb152a16df8002b
erkan <info@erkanay.com>
2014-06-24 20:14:55 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="stylesheet" href="mystyle.css" />
+</head>
+<body>
+<div class="container">
+<form action="" method="POST" >
+<textarea id="russianText" rows="8" cols="51"></textarea><br />
+	<table  class="buyukRussian">
+	  <tr align="middle"> 
+			<TD><INPUT type=button value=А></TD>
+			<TD><INPUT type=button value=Б></TD>
+			<TD><INPUT type=button value=В></TD>
+			<TD><INPUT type=button value=Г></TD>
+			<TD><INPUT type=button value=Д></TD>
+			<TD><INPUT type=button value=Е></TD>
+			<TD><INPUT type=button value=Ё></TD>
+			<TD><INPUT type=button value=Ж></TD>
+			<TD><INPUT type=button value=З></TD>
+			<TD><INPUT type=button value=И></TD>
+			<TD><INPUT type=button value=Й></TD>
+			<TD><INPUT type=button value=К></TD>
+			<TD><INPUT type=button value=Л></TD>
+			<TD><INPUT type=button value=М></TD>
+			<TD><INPUT type=button value=Н></TD>
+			<TD><INPUT type=button value=О></TD>
+			<TD><INPUT type=button value=П></TD>
+		</tr>
+	  <tr align="middle">
+			<TD><INPUT type=button value=а></TD>
+			<TD><INPUT type=button value=б></TD>
+			<TD><INPUT type=button value=в></TD>
+			<TD><INPUT type=button value=г></TD>
+			<TD><INPUT type=button value=д></TD>
+			<TD><INPUT type=button value=е></TD>
+			<TD><INPUT type=button value=ё></TD>
+			<TD><INPUT type=button value=ж></TD>
+			<TD><INPUT type=button value=з></TD>
+			<TD><INPUT type=button value=и></TD>
+			<TD><INPUT type=button value=й></TD>
+			<TD><INPUT type=button value=к></TD>
+			<TD><INPUT type=button value=л></TD>
+			<TD><INPUT type=button value=м></TD>
+			<TD><INPUT type=button value=н></TD>
+			<TD><INPUT type=button value=о></TD>
+			<TD><INPUT type=button value=п></TD>
+		</tr>
+	<tr align="middle">
+		<TD colspan="17" style="height:9px;"></TD>
+	</tr>
+	  <tr align="middle">
+			<TD><INPUT type=button value=Р></TD>
+			<TD><INPUT type=button value=С></TD>
+			<TD><INPUT type=button value=Т></TD>
+			<TD><INPUT type=button value=У></TD>
+			<TD><INPUT type=button value=Ф></TD>
+			<TD><INPUT type=button value=Х></TD>
+			<TD><INPUT type=button value=Ц></TD>
+			<TD><INPUT type=button value=Ч></TD>
+			<TD><INPUT type=button value=Ш></TD>
+			<TD><INPUT type=button value=Щ></TD>
+			<TD><INPUT type=button value=Ъ></TD>
+			<TD><INPUT type=button value=Ы></TD>
+			<TD><INPUT type=button value=Ь></TD>
+			<TD><INPUT type=button value=Э></TD>
+			<TD><INPUT type=button value=Ю></TD>
+			<TD><INPUT type=button value=Я></TD>
+			<TD><INPUT type=button value=«></TD>	
+		</tr>
+	  <tr align="middle">
+			<TD><INPUT type=button value=р></TD>
+			<TD><INPUT type=button value=с></TD>
+			<TD><INPUT type=button value=т></TD>
+			<TD><INPUT type=button value=у></TD>
+			<TD><INPUT type=button value=ф></TD>
+			<TD><INPUT type=button value=х></TD>
+			<TD><INPUT type=button value=ц></TD>
+			<TD><INPUT type=button value=ч></TD>
+			<TD><INPUT type=button value=ш></TD>
+			<TD><INPUT type=button value=щ></TD>
+			<TD><INPUT type=button value=ъ></TD>
+			<TD><INPUT type=button value=ы></TD>
+			<TD><INPUT type=button value=ь></TD>
+			<TD><INPUT type=button value=э></TD>
+			<TD><INPUT type=button value=ю></TD>
+			<TD><INPUT type=button value=я></TD>
+			<TD><INPUT type=button value=»></TD>	
+		</tr>
+		<input type="button" id="setText" value="Represent " />
+		<input type="button" id="makeClear" value="Clear Text" />
+	</table>
+	<div id="result"></div>
+</form>
+</div>
+<script src="myscript.js"></script>
+</body>
+</html>
